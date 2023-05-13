1fa5cbf91375d1c74d7c69c2da71facb35f29aa3
CookedMelon <cc2414635846@163.com>
2023-05-13 21:52:46 +0800

    1

---
+++ 出题/1/1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+int main()
+{
+    setvbuf(stdout, 0, 2, 0);
+    setvbuf(stdin, 0, 2, 0);
+    puts("My cat is very cute! I want to show it to you!");
+    printf("< Go and see ? >(y/n)");
+    char c;
+    scanf("%c", &c);
+    if (c == 'y')
+    {
+        puts("OK,go with me!");
+        // 延时1秒
+        sleep(1);
+        printf("...");
+        sleep(1);
+        printf("...");
+        sleep(1);
+        puts("...");
+        puts("Oh Shit, my cat is gone!");
+        puts("But I want to give you the shell, go and find the flag!");
+        system("/bin/sh");
+    }
+    else
+    {
+        printf("OK, I will show it to you next time!");
+        return 0;
+    }
+    return 0;
+}
